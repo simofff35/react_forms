eebc81b59dffeaf66c1b299816cf62e312df0d78
simofff35 <simofff0@gmail.com>
2023-05-02 16:41:37 +0100

    complex event 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,53 @@
+import { set } from "mongoose";
 import React, { useState } from "react";
 
 function App() {
-  let [name, setName] = useState("");
-  let [headingText, setheadingText] = useState("");
+  let [fullN, setFullN] = useState({
+    fName: "",
+    lName: "",
+  });
 
-  function changeHandle(event) {
-    setName(event.target.value);
-    console.log(name);
-  }
+  function fullNHandler(event) {
+    const newvalue = event.target.value;
+    const inputName = event.target.name;
 
-  function clickHandle() {
-    setheadingText(name);
+    setFullN((prevValue) => {
+      console.log(prevValue);
+      if (inputName === "fName") {
+        console.log(prevValue);
+        return {
+          fName: newvalue,
+          lName: prevValue.lName,
+        };
+      } else if (inputName === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: newvalue,
+        };
+      }
+    });
   }
 
   return (
     <div className="container">
-      <h1>{headingText} </h1>
-      <input
-        type="text"
-        placeholder="What's your name?"
-        onChange={changeHandle}
-      />
-      <button onClick={clickHandle}>Submit</button>
+      <h1>
+        {fullN.fName} {fullN.lName}
+      </h1>
+      <form>
+        <input
+          name="fName"
+          placeholder="First Name"
+          onChange={fullNHandler}
+          value={fullN.fName}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          onChange={fullNHandler}
+          value={fullN.lName}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
